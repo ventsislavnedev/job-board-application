9130232359cc6149f04cd274b2f856d929635b39
Ventsislav Nedev <ventsislavnedev@Ventsislavs-MacBook-Air.local>
2025-11-16 19:02:20 +0200

    Initial project files

---
+++ Api/Controllers/JobsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBoardApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JobsController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello world";
+    }
+}
---
+++ Domain/ValueObject/Salary.cs
@@ -0,0 +1,6 @@
+namespace Domain.ValueObject;
+
+public class Salary
+{
+    
+}
